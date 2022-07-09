45f59d6df7a9b9232040b0c529bca44cbd1c7513
Alek Jonas <alekjonas@hotmail.com>
2022-07-09 15:16:34 -0400

    This is the Collab Project!

---
+++ public/scripts.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    console.log("scripts have been loaded!");
+};
+
+// window.addEventListener("Load", () => {
+//     console.log("Scripts have been super loaded!!");
+//     };
